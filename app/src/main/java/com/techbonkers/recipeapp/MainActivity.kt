cf8edb09b99d6cfbeae821e4702dca6c332af02e
aunuj <aunujain53@gmail.com>
2023-11-02 18:52:27 +0530

    splash screen

--- app/src/main/java/com/techbonkers/recipeapp/MainActivity.kt
+++ app/src/main/java/com/techbonkers/recipeapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.techbonkers.recipeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            kotlin.run {
+                intent = Intent(this,HomeActivity::class.java)
+                startActivity(intent)
+            }
+            },5000)
     }
 }
